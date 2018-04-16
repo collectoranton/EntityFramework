a5ac5d618a63cd75e0e901840df7d0cb7aca775e
collectoranton <anton.lilja@collectorbank.se>
2018-04-16 15:36:46 +0200

    First commit

---
+++ EfSamurai.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using EfSamurai.Domain;
+
+namespace EfSamurai.Data
+{
+    class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = EfSamurai; Trusted_Connection = True; ");
+        }
+    }
+}
